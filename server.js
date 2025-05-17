a1221662b3b9baf62f0792d9e21512e13a9cdbf4
thecvarchitect <sales.thecvarchitect@gmail.com>
2025-05-17 15:29:04 +0300

    Add CORS support for GitHub Pages.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
+// Enable CORS for requests from GitHub Pages
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://thecvarchitect.github.io');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 const paymentStatuses = new Map();
